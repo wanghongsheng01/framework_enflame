c53c03bace034f00e88230afe9d8670be5f49159
guo-ran <360112263@qq.com>
2021-07-22 19:27:34 +0800

    rm

--- oneflow/core/job/collective_boxing_executor.cpp
+++ oneflow/core/job/collective_boxing_executor.cpp
@@ -376,7 +376,7 @@ void NcclCollectiveBoxingExecutorBackend::ExecuteGroup(
           //OF_NCCL_CHECK(ncclAllReduce(send_buff, recv_buff, elem_cnt, nccl_data_type,
           //                            GetNcclReduceOp(op_desc.reduce_method()), comm,
           //                            device_ctx->stream));
-          Memcpy<DeviceType::kGPU>(device_ctx, recv_buff, send_buff, elem_cnt * sizeof(float));
+          //Memcpy<DeviceType::kGPU>(device_ctx, recv_buff, send_buff, elem_cnt * sizeof(float));
         } else if (op_type == OpType::kOpTypeAllGather) {
           CHECK_EQ(elem_cnt % num_ranks, 0);
           OF_NCCL_CHECK(ncclAllGather(send_buff, recv_buff, elem_cnt / num_ranks, nccl_data_type,
